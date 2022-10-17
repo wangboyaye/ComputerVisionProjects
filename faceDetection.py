f9138f986eae6c5d391a8cf6c89cbe1a820ea1c0
AbelZW <38753176+abelzy@users.noreply.github.com>
2022-10-17 18:35:16 +0800

    Update faceDetection.py

--- faceDetection.py
+++ faceDetection.py
@@ -4,26 +4,17 @@ import time
 
 faceDetector = mp.solutions.face_detection
 drawing = mp.solutions.drawing_utils
-
-
 # For webcam input:
 cap = cv2.VideoCapture(0)
-
 with faceDetector.FaceDetection(
-
     min_detection_confidence=0.5) as face_detection:
-
   while cap.isOpened():
-
     success, image = cap.read()
-
     start = time.time()
-
     if not success:
       print("Ignoring empty camera frame.")
       # If loading a video, use 'break' instead of 'continue'.
       continue
-
     # Convert the BGR image to RGB.
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
@@ -39,18 +30,13 @@ with faceDetector.FaceDetection(
     if results.detections:
       for id, detection in enumerate(results.detections):
         drawing.draw_detection(image, detection)
-
     end = time.time()
     totalTime = end - start
 
     fps = 1 / totalTime
     print("FPS: ", fps)
-
     cv2.putText(image, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
-
-    cv2.imshow('MediaPipe Face Detection', image)
-
+    cv2.imshow('Face Detection', image)
     if cv2.waitKey(5) & 0xFF == 27:
       break
-
 cap.release()
